642b5780bde070174ce06910a7cdd0e00bde4021
Learningisrunning <tom.dumerle@gmail.com>
2023-05-25 15:54:59 +0200

    modif of admin

--- lettings/admin.py
+++ lettings/admin.py
@@ -3,5 +3,21 @@ from lettings.models import Letting
 from lettings.models import Address
 # Register your models here.
 
-admin.site.register(Letting)
-admin.site.register(Address)
+#admin.site.register(Letting)
+#admin.site.register(Address)
+
+@admin.register(Address)
+class AdressAdmin(admin.ModelAdmin):
+    list_display = ('number', 'street','city', 'zip_code')
+    ordering = ('city',)
+    search_fields = ('city', 'zip_code')
+    list_filter = ('city', 'street', 'zip_code', 'number')
+
+@admin.register(Letting)
+class LettingAdmin(admin.ModelAdmin):
+    list_display = ('title', 'address')
+    ordering = ('address',)
+    search_fields = ('title', 'address')
+    list_filter = ('title', 'address')
+
+
--- profiles/admin.py
+++ profiles/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from profiles.models import Profile
 # Register your models here.
 
-admin.site.register(Profile)
+#admin.site.register(Profile)
+
+
+@admin.register(Profile)
+class LettingAdmin(admin.ModelAdmin):
+    list_display = ('user', 'favorite_city')
+    ordering = ('user',)
+    search_fields = ('user', 'favorite_city')
+    list_filter = ('user', 'favorite_city')
